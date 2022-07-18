77cdccc6242a367aee91eb54e4f1a2f70ae9f7e3
Mandy Mason <mnmason86@gmail.com>
2022-07-18 18:50:51 -0400

    completed challenge 6

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -113,8 +113,10 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
-  // Solution code here...
-
+  for(let i = 0; i < arr2.length; i++){
+    let x = arr2[i];
+    arr1.push(x);
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -174,7 +176,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
