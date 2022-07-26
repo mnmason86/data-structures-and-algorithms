331d24dd1bbd9838d264c71eb42d7df43255b7e2
Mandy Mason <mnmason86@gmail.com>
2022-07-26 15:44:42 -0400

    completed challenge 5

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -63,7 +63,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  return (/^[A-Za-z0-9]+\.?[A-Za-z0-9]+@[A-Za-z0-9]+\.(net|com|org)$/).test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
