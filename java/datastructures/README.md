33512b4b1f8f3b5462da3acd900cc4cbe429ef86
Mandy Mason <mnmason86@gmail.com>
2022-08-19 20:23:29 -0400

    completed toString testing, updated README

--- java/datastructures/README.md
+++ java/datastructures/README.md
@@ -4,8 +4,14 @@
 
 ## Code Challenges
 
-### Challenge 01
+### Code Challenge 05
 
 Location: `codechallenges/linkedlist/LinkedListChallenges`
 
-Method: `linkedListChallenge06()`
+Methods:
+`insert()` - Adds a new node with the given value to the head of the list.
+
+`includes()` - Indicates whether the given value exists as a Node's value somewhere within the list.
+
+`toString()` = (Override) Returns a string representing all the values in the Linked List
+
--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -13,17 +13,27 @@ public class LinkedListTest
       System.out.println(sut);
       assertTrue(true);
     }
+    @Test void toStringTest(){
+    LinkedList<Integer> LL = new LinkedList<Integer>();
+      LL.insert(4);
+      LL.insert(9);
+      LL.insert(0);
+      LL.insert(12);
 
-  @Test void insertTest(){
+      String stringTest = LL.toString();
+
+      assertTrue(stringTest.contains("{ 9 } -> { 4 }"));
+    }
+
+    @Test void insertTest(){
     //ADD
     LinkedList<Integer> LL = new LinkedList<Integer>();
     //ACT
     LL.insert(2);
     //ASSERT
     Assert.assertTrue(LL.head.value == 2);
-
-  }
-  @Test void includes(){
+    }
+    @Test void includes(){
     LinkedList<Integer> LL = new LinkedList<>();
     LL.insert(4);
     LL.insert(9);
