43f80528fcd31745a671ff935152371430f803be
Mandy Mason <mnmason86@gmail.com>
2022-08-26 20:08:46 -0400

    re-added stacks and queues classes and methods

--- java/datastructures/lib/src/main/java/datastructures/stacks/Node.java
+++ java/datastructures/lib/src/main/java/datastructures/stacks/Node.java
@@ -1,4 +1,14 @@
 package datastructures.stacks;
 
-public class Node {
+public class Node<T> {
+  public T value;
+  public datastructures.queues.Node<T> next = null;
+
+  public Node (T value){
+    this.value = value;
+  }
+
+  public Node(){
+
+  }
 }
--- java/datastructures/lib/src/main/java/datastructures/stacks/Stack.java
+++ java/datastructures/lib/src/main/java/datastructures/stacks/Stack.java
@@ -1,4 +1,43 @@
 package datastructures.stacks;
 
-public class Stack {
+import java.util.EmptyStackException;
+
+  public class Stack<T> {
+    private Node<T> top = null;
+    public int stackSize = 0;
+
+    public int getStackSize(){
+      return stackSize;
+    }
+
+
+    public void push (T value){
+      Node<T> newNode = new Node<>(value);
+      newNode.next = top;
+      top = newNode;
+      stackSize++;
+    }
+
+    public T pop(){
+      Node<T> temp = top;
+      if (top == null){
+        throw new EmptyStackException();
+      }
+      top = top.next;
+      stackSize--;
+      return temp.value;
+    }
+
+    public T peek(){
+      if (top == null){
+        throw new EmptyStackException();
+      }
+      return top.value;
+    }
+
+    public boolean isEmpty(){
+      return stackSize == 0;
+    }
+
 }
+
