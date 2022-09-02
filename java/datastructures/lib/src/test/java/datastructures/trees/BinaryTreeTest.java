2a2b50ed63d958d674038287544ee4231434d6d0
Mandy Mason <mnmason86@gmail.com>
2022-09-02 18:29:20 -0400

    passing tests for Binary Tree class

--- java/datastructures/lib/src/test/java/datastructures/trees/BinaryTreeTest.java
+++ java/datastructures/lib/src/test/java/datastructures/trees/BinaryTreeTest.java
@@ -1,4 +1,20 @@
 package datastructures.trees;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class BinaryTreeTest {
+
+  BinaryTree<Integer> sut = new BinaryTree<Integer>();
+  @Test
+  public void instantiateEmptyTest(){
+    assertNotNull(sut);
+  }
+  @Test
+  public void instantiateWithRootTest(){
+    sut.root = new TNode<>(9);
+    assertTrue(sut.root.value == 9);
+  }
 }
